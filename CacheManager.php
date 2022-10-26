16aa52966a0ffce905539a1ae7f3dcd4c5570387
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2022-10-26 13:41:30 +0330

    base implementation

---
+++ CacheManager.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Cache;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+/* It's a class that manages the cache. */
+class CacheManager
+{
+    /**
+     * > Get a value from the cache
+     *
+     * @param string $key The key to store the value under.
+     * @param string $config The name of the configuration to use.
+     * @return mixed The value of the key in the cache.
+     */
+    public static function get(string $key, string $config = 'default'): mixed
+    {
+        /* It's getting the prefix of the cache. */
+        $key = config('cache.configs.' . $config . '.prefix') . $key;
+
+        return Cache::get($key);
+    }
+
+    /**
+     * It stores a value in the cache for a given key.
+     *
+     * @param string $key The key to store the value under.
+     * @param mixed $value The value to store.
+     * @param string $config The name of the configuration to use.
+     * @return bool A boolean value.
+     */
+    public static function put(string $key, mixed $value, string $config = 'default'): bool
+    {
+        /* It's getting the duration of the cache. */
+        $ttl = Carbon::parse(
+            config('cache.configs.' . $config . '.duration')
+        );
+
+        /* It's getting the prefix of the cache. */
+        $key = config('cache.configs.' . $config . '.prefix') . $key;
+
+        return Cache::put($key, $value, $ttl);
+    }
+
+    /**
+     * It deletes a cache
+     *
+     * @param string $key The key of the cache.
+     * @param string $config The name of the configuration to use.
+     * @return bool A boolean value.
+     */
+    public static function delete(string $key, string $config = 'default'): bool
+    {
+        /* It's getting the prefix of the cache. */
+        $key = config('cache.configs.' . $config . '.prefix') . $key;
+
+        return Cache::forget($key);
+    }
+}
