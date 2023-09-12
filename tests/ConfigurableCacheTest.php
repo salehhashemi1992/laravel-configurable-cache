413ab5e7e124f46de4f963d41799b250a332ab83
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-09-12 23:40:16 +0330

    add more functional tests

--- tests/ConfigurableCacheTest.php
+++ tests/ConfigurableCacheTest.php
@@ -24,4 +24,40 @@ class ConfigurableCacheTest extends BaseTest
 
         $this->assertEquals('testValue', $value);
     }
+
+    /** @test */
+    public function test_delete_function_removes_value_from_cache()
+    {
+        ConfigurableCache::put('testKey', 'testValue', 'tiny');
+
+        ConfigurableCache::delete('testKey', 'tiny');
+
+        $value = ConfigurableCache::get('testKey', 'tiny');
+
+        $this->assertNull($value);
+    }
+
+    /** @test */
+    public function test_increment_function_increments_cache_value()
+    {
+        ConfigurableCache::put('testKey', 1);
+
+        ConfigurableCache::increment('testKey');
+
+        $cacheValue = ConfigurableCache::get('testKey');
+
+        $this->assertEquals(2, $cacheValue);
+    }
+
+    /** @test */
+    public function test_remember_function_stores_closure_result_in_cache()
+    {
+        ConfigurableCache::remember('testKey', 'default', function () {
+            return 'testValue';
+        });
+
+        $value = ConfigurableCache::get('testKey');
+
+        $this->assertEquals('testValue', $value);
+    }
 }
